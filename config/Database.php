a021bd9830f36050d3d59bcf2859a6ff8b7cf9ee
Christian Richardson <sillarcode@gmail.com>
2025-10-27 17:09:23 -0400

    update project

--- config/Database.php
+++ config/Database.php
@@ -8,11 +8,6 @@ function getDatabaseConfig()
                 "url" => "mysql:host=localhost:3306;dbname=eshop_test",
                 "username" => "root",
                 "password" => "Sillarmysql123!"
-            ],
-            "prod" => [
-                "url" => "mysql:host=localhost:3306;dbname=eshop",
-                "username" => "root",
-                "password" => "Sillarmysql123!"
             ]
         ]
     ];
