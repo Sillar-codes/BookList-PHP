534e6601cb0d5d19159aa849ba40ccc10e015ef2
Christian Richardson <sillarcode@gmail.com>
2025-10-27 17:11:48 -0400

    fix the database config
    Co-authored-by: Lucas-coincodes0313 lucas.coincodes0313@gmail.com

--- config/Database.php
+++ config/Database.php
@@ -8,6 +8,9 @@ function getDatabaseConfig()
                 "url" => "mysql:host=localhost:3306;dbname=eshop_test",
                 "username" => "root",
                 "password" => "Sillarmysql123!"
+            ],
+            "prod" => [
+                "password" => "Sillarmysql123!"
             ]
         ]
     ];
